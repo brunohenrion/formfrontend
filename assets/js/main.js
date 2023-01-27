87607d5f66099fa6c12aadec4f60e27df725b890
HENRION <nono@MacBook-Pro-de-HENRION.local>
2023-01-27 19:55:13 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Content Loaded");
+
+  //   Gestion du formulaire
+
+  document.querySelector("form").addEventListener("submit", async (event) => {
+    // empêcher le refresh de la page au submit
+    event.preventDefault();
+    console.log("submit");
+
+    const firstname = document.querySelector("#firstname").value;
+    const lastname = document.querySelector("#lastname").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    console.log({
+      firstname,
+      lastname,
+      email,
+      message,
+    });
+
+    // réquête vers le serveur en local
+    const { data } = await axios.post(
+      "site--backend-formulaire--fwfhyq6z5d8f.code.run",
+      {
+        firstname,
+        lastname,
+        email,
+        message,
+      }
+    );
+
+    console.log("response>>", data);
+  });
+});
