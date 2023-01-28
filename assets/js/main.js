de2a5d3d21f9dcc08c70bc2b636149a70d25dcf0
HENRION <nono@MacBook-Pro-de-HENRION.local>
2023-01-28 14:26:14 +0100

    derniere modif

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,42 @@
-const axios = require("axios");
-
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Content Loaded");
 
   //Gestion du formulaire
 
-  document.querySelector("form").addEventListener("submit", async (event) => {
-    // empêcher le refresh de la page au submit
-    event.preventDefault();
-    console.log("submit");
+  document
+    .querySelector("form")
+    .addEventListener("submit", async (event, res) => {
+      try {
+        // empêcher le refresh de la page au submit
+        event.preventDefault();
+        console.log("submit");
 
-    const firstname = document.querySelector("#firstname").value;
-    const lastname = document.querySelector("#lastname").value;
-    const email = document.querySelector("#email").value;
-    const message = document.querySelector("#message").value;
+        const firstname = document.querySelector("#firstname").value;
+        const lastname = document.querySelector("#lastname").value;
+        const email = document.querySelector("#email").value;
+        const message = document.querySelector("#message").value;
 
-    console.log({
-      firstname,
-      lastname,
-      email,
-      message,
-    });
+        console.log({
+          firstname,
+          lastname,
+          email,
+          message,
+        });
 
-    // réquête vers le serveur en local
-    const { data } = await axios.post(
-      "https://site--backform-project--zywd4pc2lg9z.code.run",
-      {
-        firstname,
-        lastname,
-        email,
-        message,
+        // réquête vers le serveur en local
+        const { data } = await axios.post(
+          "https://site--backform-project--zywd4pc2lg9z.code.run/form",
+          {
+            firstname,
+            lastname,
+            email,
+            message,
+          }
+        );
+
+        console.log("response>>", data);
+      } catch (error) {
+        error: message;
       }
-    );
-
-    console.log("response>>", data);
-  });
+    });
 });
